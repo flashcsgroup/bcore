42c8bf2450ee7bb433d6e10488cebf1d28f8659a
Farruh Tashbulov <farruh.tashbulov@sunet.uz>
2015-03-27 13:40:13 +0500

    1.added docs 2.added json jars

--- src/main/java/uz/sunet/bcore/pharma/web/controller/SampleController.java
+++ src/main/java/uz/sunet/bcore/pharma/web/controller/SampleController.java
@@ -1,19 +1,18 @@
 package uz.sunet.bcore.pharma.web.controller;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * This is sample controller should be renamed for real controller
+ *
  * @author Farruh Tashbulov
  */
 @RestController
-@RequestMapping("/service/")
+@RequestMapping(value = "/service/")
 public class SampleController {
     @RequestMapping(value = "/{name}", method = RequestMethod.GET)
     public String getGreeting(@PathVariable String name) {
-        return "Hello " + name;
+        return "Hello "+name;//TODO: should be dto here and auto converted to JSON, now it not working
     }
 }
