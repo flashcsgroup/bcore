15575a7f4f72596a8b6cdced7e41a55a8882c8ca
Farruh Tashbulov <farruh.tashbulov@sunet.uz>
2015-03-30 10:20:43 +0500

    1.added suppport of json generation for response

--- src/main/java/uz/sunet/bcore/pharma/web/controller/SampleController.java
+++ src/main/java/uz/sunet/bcore/pharma/web/controller/SampleController.java
@@ -3,6 +3,10 @@ package uz.sunet.bcore.pharma.web.controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.io.Serializable;
+
 /**
  * This is sample controller should be renamed for real controller
  *
@@ -12,7 +16,35 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(value = "/service/")
 public class SampleController {
     @RequestMapping(value = "/{name}", method = RequestMethod.GET)
-    public String getGreeting(@PathVariable String name) {
-        return "Hello "+name;//TODO: should be dto here and auto converted to JSON, now it not working
+    public Message getGreeting(@PathVariable String name) {
+//       return "Hello "+name;//TODO: should be dto here and auto converted to JSON, now it not working
+        return new Message(name, "Hello " + name);
+    }
+    static class Message implements Serializable{
+        private String name;
+        private String value;
+
+        Message() {
+        }
+
+        Message(String name, String value) {
+            this.name = name;
+            this.value = value;
+        }
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+        public String getValue() {
+            return value;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+        }
+
     }
 }
